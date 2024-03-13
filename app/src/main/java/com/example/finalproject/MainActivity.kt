8bdeae7bd4f400081a39997023a0654088307a8c
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-03-13 16:12:26 -0500

    Added game entry placeholders.

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.finalproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
@@ -33,10 +41,15 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.finalproject.ui.theme.FinalProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -121,10 +134,65 @@ fun AppScaffold() {
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            // Game Entry Composable to go here.
-            Text(
-                text = "Game Entry placeholder."
+            LazyColumn {
+                item{
+                    // GameListEntry Composable to go here.
+                    GameListEntry()
+                    GameListEntry()
+                    GameListEntry()
+                    GameListEntry()
+                }
+            }
+        }
+    }
+}
+
+// Using as a placeholder until we get actual data in.
+@Composable
+fun GameListEntry(/* To add Game object in parameters */ modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
+            .padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally // This centers the content horizontally
+    ) {
+        Text(
+            text = "Call of Duty: Warzone",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 8.dp, bottom = 1.dp),
+            fontSize = 20.sp
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.thumbnail_example),
+                contentDescription = "Call of Duty Warzone title image.",
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(8.dp),
             )
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Genre: Shooter"
+                )
+                Text(
+                    text = "Platform: Windows"
+                )
+                Text(
+                    text = "Release: 2020"
+                )
+            }
         }
     }
 }
